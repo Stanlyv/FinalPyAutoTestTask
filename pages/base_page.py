fdd1a498ddcd5bfbb9eccdc34adcc95088adc570
Stanlyv <SoftPillow1337>
2023-03-18 20:53:52 +0200

    + test for login_page + changes related to adding tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
 url = ""
 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=3):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            print("\nНОУ САЧ ЕЛЕМЕНТ ЕКСЕПШН Ю НОУ")
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,25 @@
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
-link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    browser.find_element(By.CSS_SELECTOR, "#login_link").click()
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_should_see_current_url(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.go_to_login_page()
+    page = LoginPage(browser, url)
+    page.should_be_login_page()
